830ad92260f5ebbb20159ea9a03cb94c158c9419
NguVD99 <vungu10031999@gmail.com>
2025-03-05 17:12:30 +0800

    header and footer

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,25 +1,10 @@
 const express = require('express');
-const { getHomepage, getABC, getTest, postCreateUser, getCreatePage, getEditPage, postUpdateUser, postDeleteUser, postHandleRemoveUser } = require('../controllers/homeController')
+const { getHomepage } = require('../controllers/homeController')
 const router = express.Router();
 
 
 router.get('/', getHomepage)
 
-router.get('/abc', getABC)
-
-router.get('/test', getTest)
-
-router.get('/create', getCreatePage)
-
-router.get('/edit/:id', getEditPage)
-
-router.post('/create-user', postCreateUser)
-
-router.post('/update-user', postUpdateUser)
-
-router.post('/delete-user/:id', postDeleteUser)
-
-router.post('/delete-user', postHandleRemoveUser)
 
 
 module.exports = router;
